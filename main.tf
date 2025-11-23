b30f3cd6370ca1f76e672f917c6eb8887908d90c
Firoj Ahamed <fahamed@gmail.com>
2025-11-23 19:46:07 +0000

    eks-managed-custom-template-al2to-al2023

---
+++ main.tf
@@ -0,0 +1,349 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+#####################
+# Networking (VPC)  #
+#####################
+
+resource "aws_vpc" "this" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "eks-demo-vpc"
+  }
+}
+
+# Public subnets (for NAT, ALBs, etc.)
+resource "aws_subnet" "public1" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                     = "eks-public-1"
+    "kubernetes.io/role/elb" = "1"
+  }
+}
+
+resource "aws_subnet" "public2" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                     = "eks-public-2"
+    "kubernetes.io/role/elb" = "1"
+  }
+}
+
+# Private subnets for EKS nodes
+resource "aws_subnet" "private1" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name                              = "eks-private-1"
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+
+resource "aws_subnet" "private2" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name                              = "eks-private-2"
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "eks-demo-igw"
+  }
+}
+
+# Public route table: IGW
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name = "eks-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public.id
+}
+
+# NAT Gateway for private subnets
+resource "aws_eip" "nat" {
+
+  tags = {
+    Name = "eks-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public1.id
+
+  tags = {
+    Name = "eks-nat-gw"
+  }
+
+  depends_on = [aws_internet_gateway.this]
+}
+
+# Private route table: Internet via NAT
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this.id
+  }
+
+  tags = {
+    Name = "eks-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private1" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private2" {
+  subnet_id      = aws_subnet.private2.id
+  route_table_id = aws_route_table.private.id
+}
+
+###########################
+# Security Group for EKS  #
+###########################
+
+resource "aws_security_group" "eks_cluster" {
+  name        = "eks-cluster-sg"
+  description = "Cluster communication with worker nodes"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    description = "Allow worker nodes to communicate with the cluster API Server"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-cluster-sg"
+  }
+}
+
+#########################
+# IAM for EKS Cluster   #
+#########################
+
+data "aws_iam_policy_document" "eks_cluster_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "eks_cluster" {
+  name               = "eks-cluster-role"
+  assume_role_policy = data.aws_iam_policy_document.eks_cluster_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
+  role       = aws_iam_role.eks_cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSServicePolicy" {
+  role       = aws_iam_role.eks_cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+}
+
+#########################
+# IAM for Node Group    #
+#########################
+
+data "aws_iam_policy_document" "eks_node_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "eks_node" {
+  name               = "eks-node-role"
+  assume_role_policy = data.aws_iam_policy_document.eks_node_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodePolicy" {
+  role       = aws_iam_role.eks_node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryReadOnly" {
+  role       = aws_iam_role.eks_node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKS_CNI_Policy" {
+  role       = aws_iam_role.eks_node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+###################
+# EKS Cluster     #
+###################
+
+resource "aws_eks_cluster" "this" {
+  name    = "example-eks"
+  version = "1.32"
+
+  role_arn = aws_iam_role.eks_cluster.arn
+
+  vpc_config {
+    # Give cluster access to all subnets
+    subnet_ids         = [aws_subnet.public1.id, aws_subnet.public2.id, aws_subnet.private1.id, aws_subnet.private2.id]
+    security_group_ids = [aws_security_group.eks_cluster.id]
+  }
+
+  enabled_cluster_log_types = ["api", "audit", "authenticator"]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSServicePolicy
+  ]
+}
+
+#######################################
+# AL2 EKS-optimized AMI lookup        #
+#######################################
+
+data "aws_ami" "eks_al2" {
+  owners      = ["602401143452"] # Amazon EKS AMI account
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amazon-eks-node-${aws_eks_cluster.this.version}-v*"]
+  }
+}
+
+##########################
+# Launch Template (AL2)  #
+##########################
+
+resource "aws_launch_template" "mng_lt" {
+  name_prefix   = "example-eks-mng-"
+  image_id      = data.aws_ami.eks_al2.id
+  instance_type = "t3.medium"
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+
+    ebs {
+      volume_size = 50
+      volume_type = "gp3"
+    }
+  }
+
+  metadata_options {
+    http_tokens = "optional"
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "example-eks-node"
+    }
+  }
+}
+
+#############################
+# Managed Node Group (AL2)  #
+#############################
+
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "example-mng"
+  node_role_arn   = aws_iam_role.eks_node.arn
+
+  # Use private subnets so no public IP is needed
+  subnet_ids = [
+    aws_subnet.private1.id,
+    aws_subnet.private2.id
+  ]
+
+  scaling_config {
+    desired_size = 3
+    max_size     = 5
+    min_size     = 3
+  }
+
+  launch_template {
+    id      = aws_launch_template.mng_lt.id
+    version = "$Latest"
+  }
+
+  # Custom image_id is in LT; leave ami_type unset
+  # or set to "CUSTOM" if you prefer:
+  # ami_type = "CUSTOM"
+
+  capacity_type = "ON_DEMAND"
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node_AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node_AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.node_AmazonEKS_CNI_Policy
+  ]
+}
+
